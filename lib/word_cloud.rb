17888e7c7446074f576a0b7280f40ca056affdd5
Ellie Schneiders <ems2141@gmail.com>
2014-06-10 09:36:16 -0600

    beginnings of part 2

--- lib/word_cloud.rb
+++ lib/word_cloud.rb
@@ -6,11 +6,23 @@ class WordCloud
   def sift
     h_keys = @data.keys
     results = {}
+    counter = 0
     h_keys.each do |key|
       @data[key].each do |line|
         words = line.split(" ")
         words.each do |word|
-          results[word.downcase] = {:count => 1, :people => [key]}
+          if !results.include?(word)
+            results[word.downcase] = {:count => counter + 1, :people => [key]}
+            counter = 0
+          else
+            results[word] = {
+                :count => counter += 1,
+                :people => [] << key
+            }
+            #increment counter total by 1
+            #add person's name to the people hash
+
+          end
         end
       end
     end
--- spec/word_cloud_spec.rb
+++ spec/word_cloud_spec.rb
@@ -20,4 +20,43 @@ describe WordCloud do
     expect(WordCloud.new(data).sift).to eq(output)
   end
 
+  it "sifts through key phrase data for 2 people" do
+    data = {
+        "Ila Huels" => [
+        "OPTIMIZE WEB-ENABLED SUPPLY-CHAINS",
+        "brand sexy channels",
+        "ENVISIONEER ROBUST E-COMMERCE",
+        "TRANSFORM WIRELESS ARCHITECTURES",
+        "BENCHMARK CROSS-PLATFORM PARTNERSHIPS"
+    ],
+        "Cristopher Feest" => [
+        "BENCHMARK CROSS-PLATFORM PARTNERSHIPS",
+        "brand sexy channels",
+        "BENCHMARK 24/7 PARADIGMS"
+    ]
+    }
+
+    output = {
+        "optimize" => { :count => 1, :people => ["Ila Huels"] },
+        "web-enabled" => { :count => 1, :people => ["Ila Huels"] },
+        "supply-chains" => { :count => 1, :people => ["Ila Huels"] },
+        "brand" => { :count => 2, :people => ["Ila Huels", "Cristopher Feest"] },
+        "sexy" => { :count => 2, :people => ["Ila Huels", "Cristopher Feest"] },
+        "channels" => { :count => 2, :people => ["Ila Huels", "Cristopher Feest"] },
+        "envisioneer" => { :count => 1, :people => ["Ila Huels"] },
+        "robust" => { :count => 1, :people => ["Ila Huels"] },
+        "e-commerce" => { :count => 1, :people => ["Ila Huels"] },
+        "transform" => { :count => 1, :people => ["Ila Huels"] },
+        "wireless" => { :count => 1, :people => ["Ila Huels"] },
+        "architectures" => { :count => 1, :people => ["Ila Huels"] },
+        "benchmark" => { :count => 3, :people => ["Ila Huels", "Cristopher Feest"] },
+        "cross-platform" => { :count => 2, :people => ["Ila Huels", "Cristopher Feest"] },
+        "partnerships" => { :count => 2, :people => ["Ila Huels", "Cristopher Feest"] },
+        "24/7" => { :count => 1, :people => ["Cristopher Feest"] },
+        "paradigms" => { :count => 1, :people => ["Cristopher Feest"] }
+    }
+
+    expect(WordCloud.new(data).sift).to eq(output)
+  end
+
 end
