f22fdbd56a394546f11bece2796772823b21a33b
Ellie Schneiders <ems2141@gmail.com>
2014-06-12 11:25:11 -0600

    word_cloud counts words and outputs who says the word

--- lib/word_cloud.rb
+++ lib/word_cloud.rb
@@ -16,10 +16,18 @@ class WordCloud
         words = line.downcase.split(" ")
         words.each do |word|
           if results[word] == nil
-            results[word] = {:count => 1}
+            results[word] = {:count => 1, :people => [key]}
           else
             counter = results[word][:count] + 1
-            results[word] = {:count => counter}
+            people_array = results[word][:people]
+            if !people_array.include?(key)
+              x << key
+            end
+
+            #find current state/value of :people array
+            #if person isn't in array shovel the current key into the array
+            results[word][:count] = counter
+            results[word][:people] = people_array
           end
         end
       end
--- spec/word_cloud_spec.rb
+++ spec/word_cloud_spec.rb
@@ -17,20 +17,25 @@ describe WordCloud do
     expect(word_cloud.first_person).to eq("Person One")
   end
 
-  it 'can count each word' do
+  it 'can count each word and output who said it' do
     json_data = <<-INDENTED_HEREDOC
     {
       "Person One": [
       "Word1 WORD2",
       "WORD1"
+      ],
+      "Person Two": [
+      "Word2 WORD3",
+      "WORD2"
       ]
     }
     INDENTED_HEREDOC
 
     parsed_data = JSON.parse(json_data)
     word_cloud = WordCloud.new(parsed_data)
-    expect(word_cloud.word_count).to eq({"word1" => { :count => 2},
-                                        "word2" => { :count => 1}
+    expect(word_cloud.word_count).to eq({"word1" => { :count => 2, :people => ["Person One"]},
+                                        "word2" => { :count => 3, :people => ["Person One", "Person Two"]},
+                                        "word3" => { :count => 1, :people => ["Person Two"]}
                                         })
   end
 end
