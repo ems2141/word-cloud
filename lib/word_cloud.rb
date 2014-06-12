c7e330c9fc5bae1c40da1836ae216434426ca1d5
Ellie Schneiders <ems2141@gmail.com>
2014-06-12 10:20:57 -0600

    word cloud can count words

--- lib/word_cloud.rb
+++ lib/word_cloud.rb
@@ -6,4 +6,24 @@ class WordCloud
   def first_person
     @data.keys.first
   end
+
+  def word_count
+    results = {}
+    data_keys = @data.keys
+    data_keys.each do |key|
+      lines = @data[key]
+      lines.each do |line|
+        words = line.downcase.split(" ")
+        words.each do |word|
+          if results[word] == nil
+            results[word] = {:count => 1}
+          else
+            counter = results[word][:count] + 1
+            results[word] = {:count => counter}
+          end
+        end
+      end
+    end
+    results
+  end
 end
--- spec/word_cloud_spec.rb
+++ spec/word_cloud_spec.rb
@@ -7,7 +7,7 @@ describe WordCloud do
     json_data = <<-INDENTED_HEREDOC
     {
       "Person One": [
-      "Word 1 WORD 2 wOrD3"
+      "Word1 WORD2 wOrD3"
       ]
     }
     INDENTED_HEREDOC
@@ -15,5 +15,22 @@ describe WordCloud do
     parsed_data = JSON.parse(json_data)
     word_cloud = WordCloud.new(parsed_data)
     expect(word_cloud.first_person).to eq("Person One")
+  end
+
+  it 'can count each word' do
+    json_data = <<-INDENTED_HEREDOC
+    {
+      "Person One": [
+      "Word1 WORD2",
+      "WORD1"
+      ]
+    }
+    INDENTED_HEREDOC
+
+    parsed_data = JSON.parse(json_data)
+    word_cloud = WordCloud.new(parsed_data)
+    expect(word_cloud.word_count).to eq({"word1" => { :count => 2},
+                                        "word2" => { :count => 1}
+                                        })
   end
 end
