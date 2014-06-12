07dab732bf71708fa0b86c22c1d321f4617ffeb6
Ellie Schneiders <ems2141@gmail.com>
2014-06-12 09:48:39 -0600

    user can find the first person in the json string

--- lib/word_cloud.rb
+++ lib/word_cloud.rb
@@ -3,29 +3,7 @@ class WordCloud
     @data = data
   end
 
-  def sift
-    h_keys = @data.keys
-    results = {}
-    counter = 0
-    h_keys.each do |key|
-      @data[key].each do |line|
-        words = line.split(" ")
-        words.each do |word|
-          if !results.include?(word)
-            results[word.downcase] = {:count => counter + 1, :people => [key]}
-            counter = 0
-          else
-            results[word] = {
-                :count => counter += 1,
-                :people => [] << key
-            }
-            #increment counter total by 1
-            #add person's name to the people hash
-
-          end
-        end
-      end
-    end
-    results
+  def first_person
+    @data.keys.first
   end
 end
--- spec/word_cloud_spec.rb
+++ spec/word_cloud_spec.rb
@@ -1,62 +1,19 @@
 require 'spec_helper'
 require_relative '../lib/word_cloud'
+require 'json'
 
 describe WordCloud do
-  it " does stuff" do
-    data = {
-        "Ila Huels" => [
-        "OPTIMIZE WEB-ENABLED SUPPLY-CHAINS",
-        "brand sexy channels"]
+  it 'returns the name of the first person' do
+    json_data = <<-INDENTED_HEREDOC
+    {
+      "Person One": [
+      "Word 1 WORD 2 wOrD3"
+      ]
     }
+    INDENTED_HEREDOC
 
-    output =  { "optimize" => { :count => 1, :people => ["Ila Huels"] },
-        "web-enabled" => { :count => 1, :people => ["Ila Huels"] },
-        "supply-chains" => { :count => 1, :people => ["Ila Huels"] },
-        "brand" => { :count => 1, :people => ["Ila Huels"] },
-        "sexy" => { :count => 1, :people => ["Ila Huels"] },
-        "channels" => { :count => 1, :people => ["Ila Huels"] },
-    }
-
-    expect(WordCloud.new(data).sift).to eq(output)
+    parsed_data = JSON.parse(json_data)
+    word_cloud = WordCloud.new(parsed_data)
+    expect(word_cloud.first_person).to eq("Person One")
   end
-
-  it "sifts through key phrase data for 2 people" do
-    data = {
-        "Ila Huels" => [
-        "OPTIMIZE WEB-ENABLED SUPPLY-CHAINS",
-        "brand sexy channels",
-        "ENVISIONEER ROBUST E-COMMERCE",
-        "TRANSFORM WIRELESS ARCHITECTURES",
-        "BENCHMARK CROSS-PLATFORM PARTNERSHIPS"
-    ],
-        "Cristopher Feest" => [
-        "BENCHMARK CROSS-PLATFORM PARTNERSHIPS",
-        "brand sexy channels",
-        "BENCHMARK 24/7 PARADIGMS"
-    ]
-    }
-
-    output = {
-        "optimize" => { :count => 1, :people => ["Ila Huels"] },
-        "web-enabled" => { :count => 1, :people => ["Ila Huels"] },
-        "supply-chains" => { :count => 1, :people => ["Ila Huels"] },
-        "brand" => { :count => 2, :people => ["Ila Huels", "Cristopher Feest"] },
-        "sexy" => { :count => 2, :people => ["Ila Huels", "Cristopher Feest"] },
-        "channels" => { :count => 2, :people => ["Ila Huels", "Cristopher Feest"] },
-        "envisioneer" => { :count => 1, :people => ["Ila Huels"] },
-        "robust" => { :count => 1, :people => ["Ila Huels"] },
-        "e-commerce" => { :count => 1, :people => ["Ila Huels"] },
-        "transform" => { :count => 1, :people => ["Ila Huels"] },
-        "wireless" => { :count => 1, :people => ["Ila Huels"] },
-        "architectures" => { :count => 1, :people => ["Ila Huels"] },
-        "benchmark" => { :count => 3, :people => ["Ila Huels", "Cristopher Feest"] },
-        "cross-platform" => { :count => 2, :people => ["Ila Huels", "Cristopher Feest"] },
-        "partnerships" => { :count => 2, :people => ["Ila Huels", "Cristopher Feest"] },
-        "24/7" => { :count => 1, :people => ["Cristopher Feest"] },
-        "paradigms" => { :count => 1, :people => ["Cristopher Feest"] }
-    }
-
-    expect(WordCloud.new(data).sift).to eq(output)
-  end
-
 end
